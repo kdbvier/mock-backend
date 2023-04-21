c48e558b418725cbce24baf8819455997dcc886b
kdbvier <kdbvier@gmail.com>
2023-04-21 14:40:41 -0700

    change

--- api/index.js
+++ api/index.js
@@ -1,17 +1,6 @@
 const app = require("./app");
 require("dotenv").config();
 const port = process.env.PORT || 3000;
-app.get("/api", (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader("Content-Type", "text/html");
-  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
-
-app.get("/api/item/:slug", (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
-});
 app.listen(port, () => {
   console.log(`Listening to port ${port}`);
 });
